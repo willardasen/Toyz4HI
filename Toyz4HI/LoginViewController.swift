f0b28bcd6442d3f9bb07a1f56b72e7e031648bed
willardasen <willard.tio@gmail.com>
2023-10-20 17:02:34 +0700

    otw tab view and regis punya validation

--- Toyz4HI/LoginViewController.swift
+++ Toyz4HI/LoginViewController.swift
@@ -14,31 +14,41 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+
     @IBOutlet weak var emailTxt: UITextField!
+    
     
     @IBOutlet weak var passwordTxt: UITextField!
     
     
     @IBAction func loginBtn(_ sender: Any) {
         
+        guard let email = emailTxt.text, !email.isEmpty else {
+            showAlert(title:"Email is empty",message: "Email must not be empty.")
+            return
+        }
+        
+        guard let password = passwordTxt.text, !password.isEmpty else {
+            showAlert(title:"Password is empty",message: "Password must not be empty.")
+            return
+        }
+        
+        if !(email.hasSuffix(".com") && email.contains("@")) {
+            showAlert(title: "Email is not valid", message: "Email must contain @ and ends with .com")
+            return
+        }
+        
+        
         
     }
     
-    
-    
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+        
+        
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
     }
-    */
-
 }
