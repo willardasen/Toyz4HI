81a74f1b71eeedb2f8003678cd9bdd1ed8aff1eb
willardasen <willard.tio@gmail.com>
2023-10-24 15:22:02 +0700

    include home page

--- Toyz4HI/RegisterViewController.swift
+++ Toyz4HI/RegisterViewController.swift
@@ -11,6 +11,7 @@ class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
 
         // Do any additional setup after loading the view.
     }
