fee5efec7ed454f5ca36071bd322ebeff4fb7c2c
willardasen <willard.tio@gmail.com>
2023-11-24 16:25:34 +0700

    ud nambah admin dll

---
+++ Toyz4HI/HomeTableViewCell.swift
@@ -0,0 +1,34 @@
+    //
+//  HomeTableViewCell.swift
+//  Toyz4HI
+//
+//  Created by prk on 11/24/23.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var gamesImg: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    @IBOutlet weak var categoryLbl: UILabel!
+    
+    @IBOutlet weak var descLbl: UILabel!
+    
+    @IBOutlet weak var priceLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Toyz4HI/HomeViewController.swift
+++ Toyz4HI/HomeViewController.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+//NB: ini kemgkinan bs ganti karna nanti pake database
+struct games {
+    var name : String?
+    var category : String?
+    var desc : String?
+    var price : Int?
+    var image : String?
+}
 
+
+class HomeViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+
+    @IBOutlet weak var tvGames: UITableView!
+    
+    
+    //NB: ini kemgkinan bs ganti karna nanti pake database
+    var arrGames = [games]()
+    
+    func initGames(){
+        arrGames.append(games(name: "God of War", category: "Adventure, RPG", desc: "BOI BOI BOI BOI BOI", price: 500000, image: "GOW"))
+        arrGames.append(games(name: "God of War", category: "Adventure, RPG", desc: "BOI BOI BOI BOI BOI", price: 500000, image: "GOW"))
+        arrGames.append(games(name: "God of War", category: "Adventure, RPG", desc: "BOI BOI BOI BOI BOI", price: 500000, image: "GOW"))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
-        // Do any additional setup after loading the view.
+        
+        initGames()
+        tvGames.dataSource = self
+        tvGames.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 210
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrGames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellGames") as! HomeTableViewCell
+        
+        cell.nameLbl.text = arrGames[indexPath.row].name
+        cell.categoryLbl.text = arrGames[indexPath.row].category
+        cell.descLbl.text = arrGames[indexPath.row].desc
+        cell.priceLbl.text = "Rp. \(arrGames[indexPath.row].price)"
+        cell.gamesImg.image = UIImage(named: arrGames[indexPath.row].image!)
+        
+        return cell
+    }
 
 }
