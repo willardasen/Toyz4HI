4e21979ac2f3e4738c220b0b263001042241ec63
willardasen <willard.tio@gmail.com>
2023-12-04 17:44:27 +0700

    cari cara utuk select image don drop down auto ap kek

--- Toyz4HI/AdminAddGameViewController.swift
+++ Toyz4HI/AdminAddGameViewController.swift
@@ -15,7 +15,50 @@ class AdminAddGameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var gameNameTxt: UITextField!
+    
+    @IBOutlet weak var categoryTxt: UITextField!
+    
+    @IBOutlet weak var descTxt: UITextField!
+    
+    @IBOutlet weak var priceTxt: UITextField!
+    
+    @IBAction func imageOptionBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addGameBtn(_ sender: Any) {
+        guard let gameName = gameNameTxt.text, !gameName.isEmpty else {
+            showAlert(title:"Game name is empty",message: "Game name must not be empty.")
+            return
+        }
+        
+        guard let category = categoryTxt.text, !category.isEmpty else {
+            showAlert(title:"Category is empty",message: "Category must not be empty.")
+            return
+        }
+        
+        guard let desc = descTxt.text, !desc.isEmpty else {
+            showAlert(title:"Description is empty",message: "Description must not be empty.")
+            return
+        }
+        
+        guard let price = priceTxt.text, !price.isEmpty else {
+            showAlert(title:"Price is empty",message: "Price must not be empty.")
+            return
+        }
+        
+        
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
     
     
 }
